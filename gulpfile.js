ac113c14823ed0865b961133266002ab08bfee01
Diogo Silva <diogo.soares.silva@gmail.com>
2020-01-20 11:51:12 +0100

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+// load modules
+const gulp = require('gulp');
+ 
+// define functions
+function sayHello(done) { // sayHello is the name of the task
+    console.log('Hello World'); // this just prints 'Hello world'
+    done();
+}
+ 
+// export tasks
+exports.hello   = sayHello;
+exports.default = sayHello;
